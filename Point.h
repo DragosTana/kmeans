d4b8d3ed8b0c138568418cf213800b4863072d3a
Dragos <dragos.tanasa99@gmail.com>
2022-10-28 16:58:28 +0200

    Create Point.h header file

---
+++ Point.h
@@ -0,0 +1,35 @@
+//
+// Created by dragos on 28/10/22.
+//
+
+#ifndef KMEANS_POINT_H
+#define KMEANS_POINT_H
+
+#include<float.h>
+
+struct Point{
+
+    double x, y;
+    int cluster;
+    double minDist;
+
+    Point() :
+        x(0.0),
+        y(0.0),
+        cluster(-1),
+        minDist(__DBL_MAX__)
+    {}
+
+    Point(double x, double y) :
+        x(x),
+        y(y),
+        cluster(-1),
+        minDist(__DBL_MAX__)
+    {}
+
+    double distance(Point p){
+        return (x-p.x)*(x-p.x)+(y-p.y)*(y-p.y);
+    }
+};
+
+#endif //KMEANS_POINT_H
